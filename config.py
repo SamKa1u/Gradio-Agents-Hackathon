8cc1fc5a8bed2fbf71409fcff7b30a48e8ef18dd
Sam <samkalu@ttu.edu>
2025-06-10 01:51:01 -0500

    working demo

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import (
+    String,
+    Integer,
+    Float,
+)
+
+class PATHS:
+    # get temp path from env
+    TEMP_PATH = os.getenv('Temp')
+    
+    TABLE_NAME = "agent_table"
+    
+    PKL_FILE_PATH = TEMP_PATH + "/TABLE_DATA.pkl"
+
